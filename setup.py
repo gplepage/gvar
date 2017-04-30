3dac3cdaf11e26af83c0e5d59d4b19a4931dfbec
G. Peter Lepage <g.p.lepage@cornell.edu>
2017-04-30 11:31:11 -0400

    prep for v8.2.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
-GVAR_VERSION = '8.2.2'
+GVAR_VERSION = '8.2.3'
 
 # create gvar/_version.py so gvar knows its version number
 with open("src/gvar/_version.py","w") as version_file:
